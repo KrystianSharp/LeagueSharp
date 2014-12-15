7a66003c4f2812364b3f66efe7b3f76d3a3ca35c
KrystianSharp <czas019@gmail.com>
2014-12-15 11:13:37 +0100

    2

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LeagueSharp;
 
 namespace Test1
 {
@@ -10,6 +11,7 @@ namespace Test1
     {
         static void Main(string[] args)
         {
+            Game.PrintChat("KP");
         }
     }
 }
